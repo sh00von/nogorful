416bbdc86c5b4e4e5ec88bab687e682b2c58ecb1
sh00von <u2110015@student.cuet.ac.bd>
2024-09-02 21:39:52 +0600

    Update

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Add favicon */}
+        <link rel="icon" href="/assets/images/nf/logo.jpg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
